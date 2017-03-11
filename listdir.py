417cea9ce88ed9481ebf6fe202f03bb729dc6907
conthor <tule@artemiscom.com>
2017-03-11 10:12:47 -0800

    Update listdir.py

--- listdir.py
+++ listdir.py
@@ -2,10 +2,10 @@ import os
 from os import walk
 
 path = "c:\\users\Tommy"
-out = open("C:\\data\\find.txt","w") 
+listf="C:\\data\\find.txt"
+out = open(listf,"w") 
 for (path, dirs, files) in os.walk(path):
     for file in files:
 		fullname = path+file
 		out.write(fullname + '\n')
 out.close
-
