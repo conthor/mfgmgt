c6a5eae616b54d353ece34efff240c015d32c4d0
conthor <tule@artemiscom.com>
2017-03-23 17:32:47 -0700

    Update readso.py

--- readso.py
+++ readso.py
@@ -1,29 +1,63 @@
+#!c:\Python27\python.exe
 import os
 import slate
 import re
 from openpyxl import load_workbook
 from openpyxl.drawing.image import Image as ImageXL
+from openpyxl.styles import Font
 import csv
 import datetime
 import barcode
 from barcode.writer import ImageWriter
 from PIL import Image
+import Tkinter,tkFileDialog
+import ctypes  # An included library with Python install.
+def Mbox(title, text, style):
+    rc = ctypes.windll.user32.MessageBoxW(0, text, title, style)
+    return rc
+##  Styles:
+##  0 : OK
+##  1 : OK | Cancel
+##  2 : Abort | Retry | Ignore
+##  3 : Yes | No | Cancel
+##  4 : Yes | No
+##  5 : Retry | No 
+##  6 : Cancel | Try Again | Continue
+
+## IDABORT 3
+## IDCANCEL 2
+## IDCONTINUE 11
+## IDIGNORE 5
+## IDNO 7
+## IDOK 1
+## IDRETRY 4
+## IDTRYAGAIN 10
+## IDYES 6
+
+
+root = Tkinter.Tk()
+root.withdraw()
+filename = tkFileDialog.askopenfilename(filetypes=[("PDF file","*.pdf")],title='Choose an SO PDF file')
+if filename != None:
+    responseno = Mbox(u'File selected', filename, 1)
+    if ( responseno == 2 ):
+        exit()
 
 travelcust={}
 travelcust["01 CTI (Cent)"]="01 CTI"
 travelcust["02 Jabil Wolf"]="02 Wolfe"
-travelcust["03 Assured MFG"]="04 TRU"
-travelcust["04 TRU Machining"]="05 OTIS"
-travelcust["05 OTIS (Ewa)"]="03 Assured"
+travelcust["03 Assured MFG"]="03 Assured"
+travelcust["04 TRU Machining"]="04 TRU"
+travelcust["05 OTIS (Ewa)"]="05 OTIS"
 travelcust["06 ThermoFisher (Ewa)"]="06 Thermo"
 travelcust["07 Vecco (Luy Danh)"]="07 Vecco"
 travelcust["08 Intevac (Luy Danh)"]="08 Intevac"
-travelcust["09 Dung Marina"]="10-C"
-travelcust["10 C.X.O (Sam)"]="09 Marina"
+travelcust["09 Dung Marina"]="09 Marina"
+travelcust["10 C.X.O (Sam)"]="10-C"
 travelcust["11 Embraer (Sam)"]="11 Embraer"
 travelcust["12 MW Technology - Cris (Sam)"]="12 MW Tech"
 
-filename = "C:\PT&R\Customers\\10.Sam\C. X.O\SO\SO 31824\SO 31824.pdf"
+#filename = "C:\PT&R\\01 Customers\\02 Jabil Wolf\\01 Job Documents\SO 31878 PO 131400-E5212T\SO 31878.pdf"
 txtfile = os.path.splitext(filename)[0]+'.txt'
 out = open(txtfile,"w") 
 with open(filename, 'rb') as f:
@@ -34,12 +68,12 @@ out.close()
 sodir = os.path.dirname(filename)
 jobdir = os.path.dirname(sodir) # new
 custdir = os.path.dirname(jobdir) # new
-traveldir = sodir # new
-custdir = os.path.dirname(sodir) # old, will be removed
-traveldir = custdir + "\\Traveler" # old, will be removed
-jobdir = custdir + "\\01 Job Documents"  # old, will be removed
-faidir = custdir + "\\FAI"
-shipdir = custdir + "\\Shipping"
+traveldir = sodir  # new
+#custdir = os.path.dirname(sodir) # old, will be removed
+#traveldir = custdir + "\\Traveler" # old, will be removed
+#jobdir = custdir + "\\01 Job Documents"  # old, will be removed
+faidir = sodir + "\\FAI"
+shipdir = sodir + "\\Shipping"
 custname = os.path.basename(custdir)
 text_file = open(txtfile, "r")
 lines = text_file.read().split('\n\n')
@@ -47,7 +81,7 @@ enum_lines = enumerate(lines)
 
 found_item_no = 0
 itemNameFound = 0
-sosize=2
+sosize=6
 if (sosize > 2):
     adj1 = 0
     adj2 = 0
@@ -74,10 +108,11 @@ for iteration in enum_lines:
         PO = lines[index+2]
         SOPO = "SO "+SO+" PO "+PO
         print servicetype, PO
-    if  (item == "Ship Method"):
+    if  (item == "Delivery Date"):
         paymentDue = lines[index+1]
         soShipDate = lines[index+2]
-        print paymentDue, soShipDate
+        loShipDate = lines[index+4]
+        print paymentDue, soShipDate, loShipDate
     if ( found_item_no > 0 ):
         if ( item.isdigit() ):
             itemNo = int(item)
@@ -113,18 +148,18 @@ for iteration in enum_lines:
 #
 # Generate Directories Traveler
 #
-dirs = [jobdir,faidir,shipdir,traveldir]
+travelsodir = traveldir + "\\Traveler" #old
+travelsodir = traveldir  #new
+dirs = [faidir,shipdir,travelsodir]
 for onedir in dirs:
-    sopodir = onedir + "\\" + SOPO
-    print sopodir
-    if not os.path.exists(sopodir):
-        os.makedirs(sopodir)
-travelsodir = sopodir
+    if not os.path.exists(onedir):
+        os.makedirs(onedir)
+
 print traveldir
 for index,partNo in enumerate(itemNames):
     print index,partNo
     lineno = index + 1
-    wb = load_workbook("c:\\data\\traveler.xlsx")
+    wb = load_workbook("c:\data\Traveler Template_Updated20170316.xlsx")
     ws = wb.active
 
     itemDescLines = itemDesc[index].split('\n')
@@ -137,24 +172,24 @@ for index,partNo in enumerate(itemNames):
             desc = itemDescLines[i]
             if re.match(r'^Mat: ', desc):
                 mtype = desc.replace("Mat: ", "", 1)
-            if re.match(r'^Material: ', desc):
-                mtype = desc.replace("Mat: ", "", 1)
-            if re.match(r'^Fin: ', desc):
-                fin = desc.replace("Fin: ", "", 1)
+            if re.match(r'^Dwg: ', desc):
+                drawno = desc.replace("Dwg: ", "", 1)
+            if re.match(r'^Finish: ', desc):
+                fin = desc.replace("Finish: ", "", 1)
         else:       
             pdescription = itemDescLines[0]
     
     shipdate=soShipDate
     duedate=paymentDue
-    quoteno="quoteno"
+    quoteno=""
     #PO="PO 123"
     jobno=SO+"-LN"+str(lineno)
-    drawno = "drawno"
-    custno = custname
+    #drawno = "drawno"
+    custno = travelcust[custname]
     qty_order = itemQty[index]
     #mtype = "M type"
     mlotno = "Lot 30304"
-    outside = servicetype
+    outside = ""
     o_vendor = ""
     o_vendor_po = ""
 
@@ -162,22 +197,34 @@ for index,partNo in enumerate(itemNames):
     ws["C2"] = pdescription
     d = datetime.datetime.now()
     only_date, only_time = d.date(), d.time()
-    ws['I1'].number_format = 'm/d/y'
-    ws["I1"] = shipdate
-    ws["I2"] = duedate
-    ws["I3"] = quoteno
-    ws["I4"] = PO
-    ws["H7"] = outside
-    ws["H8"] = o_vendor
-    ws["H9"] = o_vendor_po
+    ws['I3'].number_format = 'm/d/y'
+    ws["I3"] = shipdate
+    ws["I4"] = duedate
+    ws['I2'] = "=DATE(YEAR(I3), MONTH(I3), DAY(I3)-3)"
+    #ws["I3"] = quoteno
+    ws["I6"] = PO
+    ws["H9"] = fin
+    ws["H10"] = o_vendor
+    ws["H11"] = o_vendor_po
     ws["C5"] = jobno
     ws["C6"] = custno
     ws["C7"] = drawno
     ws["C8"] = only_date
     ws['C8'].number_format = 'm/d/y'
     ws["C10"] = qty_order
-    ws["C13"] = mtype
-    ws["C14"] = mlotno
+    ws["C14"] = mtype
+    c24 = ws['C24']
+    c25 = ws['C25']
+    c26 = ws['C26']
+    c27 = ws['C27']
+    c28 = ws['C28']
+    ft = Font(size=11)
+    c24.font = ft
+    c25.font = ft
+    c26.font = ft
+    c27.font = ft
+    c28.font = ft
+    #ws["C14"] = mlotno
     imgfile = travelsodir + "\\" + partNo
     pngfile = travelsodir + "\\" + partNo + ".png"
     CODE39 = barcode.get_barcode_class('code39')
@@ -187,13 +234,14 @@ for index,partNo in enumerate(itemNames):
     new_width  = 186
     new_height = 120
     img = img.resize((new_width, new_height), Image.ANTIALIAS)
-    img.save(fullname)
-    img1 = ImageXL(fullname)
+    #img.save(fullname)
+    img0 = ImageXL(fullname)
+    ws.add_image(img0,'A29')
+    img1 = ImageXL('c:\\data\\logo1.png')
     ws.add_image(img1,'L1')
-    #img1 = Image('c:\\data\\logo1.png')
-    #ws.add_image(img1,'L1')
-    #img2 = Image('c:\\data\\logo2.png')
-    #ws.add_image(img2,'L3')
-    traveler = travelsodir + "\\T " + partNo + ".xlsx"
+    img2 = ImageXL('c:\\data\\logo2.png')
+    ws.add_image(img2,'L4')
+    traveler = travelsodir + "\\T " + jobno + ".xlsx"
     wb.save(traveler)
-    wb.close()  
+    wb.close() 
+Mbox(filename, u'Travelers were succesfully created', 0)	
